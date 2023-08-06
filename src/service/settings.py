258c391536f4f6fb3842483c48c73ba241954659
Karbolin Ivan <karboliniv@gmail.com>
2023-08-06 14:01:01 +0300

    Actions | fix

--- src/service/settings.py
+++ src/service/settings.py
@@ -3,16 +3,16 @@ from src.enums.paths import Path
 
 
 class Settings(BaseSettings):
-    host: str
-    port: str
-    file: str
-    start: str
-    stop: str
-    restart: str
+    host: str = "localhost"
+    port: str = "9999"
+    file: str = "webcalculator.exe"
+    start: str = "start"
+    stop: str = "stop"
+    restart: str = "restart"
 
-    class Config:
-        env_file = f"{Path.ENV}"
-        env_file_encoding = "utf-8"
+    # class Config:
+    #     env_file = f"{Path.ENV}"
+    #     env_file_encoding = "utf-8"
 
 
 app_settings = Settings()
